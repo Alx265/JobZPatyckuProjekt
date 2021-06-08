fcd71283355956f19b216c48da9eb557e5c1e191
Marcin <mwawreczko265@gmail.com>
2021-06-08 18:17:33 +0200

    Code refactoring validation picture

---
+++ js/create-picture.js
@@ -0,0 +1,48 @@
+const form = document.querySelector('#create-post-picture-form');
+const title = document.querySelector('#title');
+const category = document.querySelector('#category');
+
+form.addEventListener('submit',(e) => {
+	e.preventDefault();
+
+	checkInputs();
+});
+
+function checkInputs() {
+	const titleValue = title.value.trim();
+	const categoryValue = category.value;
+
+	if(titleValue === ''){
+		setError(title,'Tytuł jest wymagany');
+	} else {
+		setSuccess(title);
+	}
+
+	if(categoryValue === ''){
+		setError(category,'Kategoria jest wymagana');
+	} else {
+		setSuccess(category);
+	}
+}
+
+function setError(input,message) {
+	const createForm = input.parentElement;
+	const small = createForm.querySelector('small');
+	small.textContent = message;  
+	createForm.className = 'create-post-control error';
+}
+
+function setSuccess(input){
+	const createForm = input.parentElement;
+	createForm.className = 'create-post-control success';
+}
+
+
+function showPreview(event) {
+	if(event.target.files.length > 0){
+		const src = URL.createObjectURL(event.target.files[0]);
+		const preview = document.querySelector("#file-image-1-preview");
+		preview.src = src;
+		preview.style.display = "block";
+	}
+}
