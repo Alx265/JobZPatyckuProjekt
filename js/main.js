d093374eaaf24e09a254023f287cbaaa14534693
Alx265 <marcin265@onet.eu>
2021-07-21 16:54:36 +0200

    Settings done

--- js/main.js
+++ js/main.js
@@ -21,3 +21,14 @@ const commentReply = document.querySelector('comment-reply');
 commentReply.addEventListener('click',() => {
 	
 });
+
+
+
+function showPreview(event) {
+	if(event.target.files.length > 0){
+		const src = URL.createObjectURL(event.target.files[0]);
+		const preview = document.querySelector("#file-image-1-preview");
+		preview.src = src;
+		preview.style.display = "block";
+	}
+}
